bd823effce9ce8df309579eb2b70b2a9cd937256
mrjak13 <justinkenney13@gmail.com>
2019-02-11 12:04:31 -0800

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,39 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const addEmployeeButton = document.querySelector('form input:last-child');
+const removeEmployeesButton = document.querySelector('a')
+const ul = document.querySelector('.employee-list');
+
+addEmployeeButton.addEventListener('click', event => {
+    addNewElementAsLi();
+    document.querySelector('form input').value = "";
+  })
+
+  removeEmployeesButton.addEventListener('click', event => {
+    let list = document.querySelectorAll('ul.employee-list li')
+    list.forEach(function(element) {
+      element.remove();
+    });
+  });
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('form input').value;
+}
+
+function addNewElementAsLi() {
+  let info = retrieveEmployeeInformation();
+  let li = document.createElement('li');
+  li.innerHTML = (info);
+  ul.appendChild(li);
+}
+
+
+function addNewLiOnClick() {
+  
+}
+
+function clearEmployeeListOnLinkClick() {
+
+}
